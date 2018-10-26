663cb76e91afcaeb7c3f919b9321323b1e5e11d1
unknown <s.medykowski@gmail.com>
2018-10-26 22:43:43 +0200

    First Commit

---
+++ src/main/java/com/kodilla/calculator/Calculator.java
@@ -0,0 +1,32 @@
+package com.kodilla.calculator;
+
+public class Calculator {
+
+    private double a;
+    private double b;
+    private double total;
+    private double difference;
+
+    private double add(double a, double b) {
+        total = a + b;
+        return total;
+    }
+
+    private double add(double a, double b, double c){
+        total = a + b + c;
+        return total;
+    }
+
+    private double subtract(double a, double b) {
+        difference = a - b;
+        return difference;
+    }
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+
+        System.out.println("total of addition a + b = " + calculator.add(5,3));
+        System.out.println("total of addition a + b + c = " + calculator.add(5,3,4));
+        System.out.println("total of subtraction a - b = " + calculator.subtract(20.5, 5));
+    }
+}
